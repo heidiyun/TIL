fceb277c8971bf5fc25a861045916d9a9a0a2909
Yunjeong Goo <heidiyun.goo@gmail.com>
2018-11-21 15:08:36 +0900

    실습 BeatBox - 21

--- android/Book-AndroidProgramming/BeatBox/app/src/main/java/kr/ac/ajou/heidi/beatbox/BeatBoxFragment.kt
+++ android/Book-AndroidProgramming/BeatBox/app/src/main/java/kr/ac/ajou/heidi/beatbox/BeatBoxFragment.kt
@@ -1,7 +1,6 @@
 package kr.ac.ajou.heidi.beatbox
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -35,6 +34,13 @@ class BeatBoxFragment : Fragment() {
         view.fragmentBeatBoxRecyclerView.layoutManager = GridLayoutManager(activity?.applicationContext!!, 3)
         recyclerViewAdapter.sounds = beatBox.sounds
         view.fragmentBeatBoxRecyclerView.adapter = recyclerViewAdapter
+
+        val theme = activity?.theme ?: return view
+        val attrsToFetch: IntArray = intArrayOf(R.attr.colorAccent)
+        val typedArray = theme.obtainStyledAttributes(R.style.AppTheme, attrsToFetch)
+        val accentColor  = typedArray.getInt(0,0)
+
+
 
         return view
     }
